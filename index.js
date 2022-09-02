7425525d495cc5e0891d8f74955f25638f009ba7
devin-lynch <devin.l.lynch@gmail.com>
2022-09-02 18:19:42 -0500

    Define a route

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 // required packages
+const express = require('express')
 
 // config an instance of express
+const app = express()
+const PORT = 3000
+app.set('view engine', 'ejs')
 
 // define some routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the Star Wars search engine')
+})
 
 // listen on a port
+app.listen(PORT, () => {
+    console.log(`aaaaaaarrrrrrrhhhghghghgghghgh`)
+})
